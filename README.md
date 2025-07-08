8c38e42bc0a8ac0ba892b0617ef0be9e5b139597
almostSouji <timoqueezle@gmail.com>
2025-07-09 00:04:22 +0200

    feat: add js script taking file path rather than reading stdin

--- README.md
+++ README.md
@@ -4,15 +4,9 @@ Format ewit csv output to be easier to read in sequence, like a chat.
 
 At its core, this is a very simple, hacky, poc of a CSV formatter aligning with the export format of ewitness.
 
+
 # Usage
 
-```sh
-cat ewit-export.csv | python3 ./format.py
-# or
-cat ewit-export.csv | ./format.py # with chmod +x ./format.py and shebang support
-```
-
-To determine the format, modify the `FORMAT` variable in `format.py`.
 
 Available template strings depend on the provided csv data, as determined by the header line. Names are used lowercase and with spaces replaced by `_`. The key `SERVER NAME` is represented by the template `{server_name}`. Due to python format string handling, braces have to be escaped, making it `f"{{server_name}}"` in the template string itself.
 
@@ -22,4 +16,27 @@ As addition the script provides the templates:
 - `{timestamp_fmt}` represents the timestamp (if available) in an easier to read format
 
 Messages are sorted by `"TIMESTAMP"` column ascending. 
+
+## Python 
+
+The python variant reads from stdin.
+
+```sh
+cat ewit-export.csv | python3 ./format.py
+# or
+cat ewit-export.csv | ./format.py # with chmod +x ./format.py and shebang support
+```
+
+To determine the format, modify the `FORMAT` variable in `format.py`.
+
+
+## Node.js
+
+The node.js variant of the script takes the file as argument, rather than processing stdin:
+
+```sh
+node format.js ./ewit-export.csv
+```
+
+To determine the format, modify the `out` variable at the end of the script.
 
---
+++ format.js
@@ -0,0 +1,92 @@
+import { readFileSync } from "node:fs";
+
+// ---------------
+
+const BLACK = "\x1b[30m";
+const RED = "\x1b[31m";
+const GREEN = "\x1b[32m";
+const YELLOW = "\x1b[33m"; // orange on some systems
+const BLUE = "\x1b[34m";
+const MAGENTA = "\x1b[35m";
+const CYAN = "\x1b[36m";
+const LIGHT_GRAY = "\x1b[37m";
+const DARK_GRAY = "\x1b[90m";
+const BRIGHT_RED = "\x1b[91m";
+const BRIGHT_GREEN = "\x1b[92m";
+const BRIGHT_YELLOW = "\x1b[93m";
+const BRIGHT_BLUE = "\x1b[94m";
+const BRIGHT_MAGENTA = "\x1b[95m";
+const BRIGHT_CYAN = "\x1b[96m";
+const WHITE = "\x1b[97m";
+
+const RESET = "\x1b[0m"; // called to return to standard terminal text color
+
+// ---------------
+
+const fileArg = process.argv[2];
+
+const file = readFileSync(fileArg, "utf-8");
+const [header, ...rest] = file.split("\n");
+
+const data = rest.join("\n");
+const headerFields = header
+	.trim()
+	.replaceAll('"', "")
+	.replaceAll(" ", "_")
+	.split(",")
+	.map((value) => value.toLowerCase());
+const pattern = headerFields
+	.map((field) => `\\"(?<${field}>[^\\"]*?)\\"`)
+	.join(",");
+
+console.log(pattern);
+const regExp = new RegExp(pattern, "gi");
+
+const messages = [];
+
+function formatDateIdentifier(identifier) {
+	return String(identifier).padStart(2, "0");
+}
+
+function formatDate(date) {
+	const prefix = [
+		date.getUTCFullYear(),
+		date.getUTCMonth() + 1,
+		date.getUTCDate(),
+	]
+		.map((identifier) => formatDateIdentifier(identifier))
+		.join("-");
+
+	const time = [date.getUTCHours(), date.getUTCMinutes()]
+		.map((identifier) => formatDateIdentifier(identifier))
+		.join(":");
+
+	return `${prefix} ${time}`;
+}
+
+let match;
+let counter = 0;
+while ((match = regExp.exec(data)) !== null) {
+	const message = { ...match.groups, row: ++counter };
+
+	const date = new Date(message.timestamp);
+	const formattedTimestamp = formatDate(date);
+
+	message["timestamp_fmt"] = formattedTimestamp;
+	message["timestamp_num"] = date.getTime();
+
+	messages.push(message);
+}
+
+messages.sort((first, second) => first.timestamp_num - second.timestamp_num);
+
+for (const message of messages) {
+	let out = `${DARK_GRAY}{row} ${BRIGHT_GREEN}{server_name} ${LIGHT_GRAY}{timestamp_fmt} ${BRIGHT_MAGENTA}{username}${RESET}: {message}`;
+
+	out = out.replaceAll(/{(.+?)}/gi, (_pattern) => {
+		const pattern = _pattern.replaceAll("{", "").replaceAll("}", "");
+		return message[pattern] ?? "-";
+	});
+
+	console.log(out);
+}
